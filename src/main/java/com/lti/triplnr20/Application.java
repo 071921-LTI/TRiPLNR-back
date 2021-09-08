ac7987ddc6323f2b43d5f2e0d50ab971a6956628
Alan Liu <55858649+liu746233077@users.noreply.github.com>
2021-09-08 16:26:30 -0400

    added Weather Controller and Weather Services

--- src/main/java/com/lti/triplnr20/Application.java
+++ src/main/java/com/lti/triplnr20/Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.lti.triplnr20.services.WeatherService;
+import com.lti.triplnr20.services.WeatherServiceImpl;
+
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -14,6 +17,9 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+//		WeatherService ws = new WeatherServiceImpl();
+//		System.out.println(ws.getCurrentWeather("Uncasville,CT"));
+//		System.out.println(ws.getDestinationWeather("Uncasville,CT","1"));
 	}
 
 	@Bean
--- src/main/java/com/lti/triplnr20/services/WeatherServiceImpl.java
+++ src/main/java/com/lti/triplnr20/services/WeatherServiceImpl.java
@@ -1,27 +1,43 @@
 package com.lti.triplnr20.services;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import com.lti.triplnr20.models.weather.WeatherJSON;
+@Service
 public class WeatherServiceImpl implements WeatherService {
 
 	@Override
 	public String getCurrentWeather(String address) {
-		// TODO Auto-generated method stub
-//		String url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+address+"?unitGroup=us";
-//		RestTemplate rt = new RestTemplate();
-//		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
-//				.queryParam("key", System.getenv("WEATHER_API_KEY"));
-//		String uri = builder.build(false).toUriString();
-//		System.out.println(uri);
-//		return url;
-		return null;
+		String url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+address+"?unitGroup=us";
+		RestTemplate rt = new RestTemplate();
+		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
+				.queryParam("key", System.getenv("WEATHER_API_KEY"));
+		String uri = builder.build(false).toUriString();
+		System.out.println(uri);
+		WeatherJSON r = rt.getForObject(uri, WeatherJSON.class);
+		String res = r.getDays().get(0).datetime + " "
+		+ String.valueOf(r.getDays().get(0).temp)+ " "
+		+r.getDays().get(0).conditions;
+		return res;
+		
 	}
 
 	@Override
 	public String getDestinationWeather(String address, String day) {
-		// TODO Auto-generated method stub
-		return null;
+		String url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+address+"?unitGroup=us";
+		RestTemplate rt = new RestTemplate();
+		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
+				.queryParam("key", System.getenv("WEATHER_API_KEY"));
+		String uri = builder.build(false).toUriString();
+		System.out.println(uri);
+		WeatherJSON r = rt.getForObject(uri, WeatherJSON.class);
+		String res = r.getDays().get(Integer.parseInt(day)).datetime + " " 
+		+String.valueOf(r.getDays().get(Integer.parseInt(day)).temp) +" "
+				+r.getDays().get(Integer.parseInt(day)).conditions;
+		return res;
 	}
 	
 
