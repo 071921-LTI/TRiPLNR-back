2ed7000bc897d8062b96a3f5f021be57326e6e38
Alan Liu <55858649+liu746233077@users.noreply.github.com>
2021-09-08 13:22:51 -0400

    added Weather Service Interface

---
+++ src/main/java/com/lti/triplnr20/services/WeatherServiceImpl.java
@@ -0,0 +1,28 @@
+package com.lti.triplnr20.services;
+
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+public class WeatherServiceImpl implements WeatherService {
+
+	@Override
+	public String getCurrentWeather(String address) {
+		// TODO Auto-generated method stub
+//		String url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+address+"?unitGroup=us";
+//		RestTemplate rt = new RestTemplate();
+//		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
+//				.queryParam("key", System.getenv("WEATHER_API_KEY"));
+//		String uri = builder.build(false).toUriString();
+//		System.out.println(uri);
+//		return url;
+		return null;
+	}
+
+	@Override
+	public String getDestinationWeather(String address, int day) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
