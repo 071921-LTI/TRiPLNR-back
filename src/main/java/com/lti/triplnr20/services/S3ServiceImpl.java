b1ba10d9089e0ab42d00e2c1509ef3798ec0e487
unknown <Lntinfotech@INFEDL31185.lntinfotech.com>
2021-09-14 12:12:25 -0500

    created s3 serviceImpl

--- src/main/java/com/lti/triplnr20/services/S3ServiceImpl.java
+++ src/main/java/com/lti/triplnr20/services/S3ServiceImpl.java
@@ -3,17 +3,34 @@ package com.lti.triplnr20.services;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.regions.Regions;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.UUID;
+
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.lti.config.AmazonConfig;
 
 @Service
 public class S3ServiceImpl implements S3Service{
 
+    private final AmazonS3 s3 = new AmazonConfig().s3();
+
     @Override
-    public String upload(MultipartFile file) {
+    public String upload(MultipartFile file) throws IOException {
         
-        return null;
+        InputStream inputStream = file.getInputStream();
+
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+
+        String uuid = UUID.randomUUID().toString();
+
+        s3.putObject("triplnr-images", uuid, inputStream, objectMetadata);
+        
+        URL imageUrl = s3.getUrl("triplnr-images", uuid);
+
+        return imageUrl.toString();
     }
     
 }
